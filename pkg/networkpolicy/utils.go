0dd2916eb24403d8825362a7d0ba24f98dd8b0a1
Antonio Ojea <antonio.ojea.garcia@gmail.com>
2021-06-11 15:47:50 +0200

    add IPtoIPNet util

--- pkg/networkpolicy/utils.go
+++ pkg/networkpolicy/utils.go
@@ -1,6 +1,8 @@
 package networkpolicy
 
 import (
+	"net"
+
 	v1 "k8s.io/api/core/v1"
 )
 
@@ -12,4 +14,15 @@ func getPodIPs(podStatus v1.PodStatus) []string {
 	}
 
 	return addresses
+}
+
+func IPtoIPNet(ip net.IP) *net.IPNet {
+	maskLen := 128
+	if ip.To4() != nil {
+		maskLen = 32
+	}
+	return &net.IPNet{
+		IP:   ip,
+		Mask: net.CIDRMask(maskLen, maskLen),
+	}
 }
